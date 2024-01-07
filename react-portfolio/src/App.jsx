60d01cdf09f9e47d20147e2f73eadfe8b38f9d11
Nihar Kadam <nihar.kadam@gmail.com>
2024-01-07 20:41:01 +0530

    Added Projects Section

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -3,15 +3,16 @@ import { About } from "./components/About/About";
 import { Dashboard } from "./components/Dashboard/Dashboard";
 import { Experience } from "./components/Experience/Experience";
 import { Navbar } from "./components/Navbar/Navbar";
-
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
-      <Dashboard/>
-      <About/>
-      <Experience/>
+      <Dashboard />
+      <About />
+      <Experience />
+      <Projects />
     </div>
   );
 }
